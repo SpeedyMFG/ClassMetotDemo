6d985ad26d8cc6bc86ad10d96955cb5e16224db5
M.Furkan Gün <furkannug34@gmail.com>
2021-02-02 11:57:54 +0300

    Müşteri-demo

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,30 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Haktan";
+            musteri1.MusteriSoyadi = "Tanrıkulu";
+            musteri1.MusteriMemleket = "Bayburt";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Muhammet Furkan";
+            musteri2.MusteriSoyadi = "Gün";
+            musteri2.MusteriMemleket = "Muş";            
+
+            Musteri[] musteriler = new Musteri[] { musteri2, musteri1 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriSoyadi);
+                Console.WriteLine(musteri.MusteriMemleket);
+                Console.WriteLine("-----------------------------");
+
+
+            }
+
+
+
         }
     }
 }
